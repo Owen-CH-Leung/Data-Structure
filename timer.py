4d50419e863ba201a8fbc5cfe5e27a963559cac9
owenleung123 <owen.leung2@gmail.com>
2021-05-10 12:00:10 +0800

    Create timer.py

---
+++ timer.py
@@ -0,0 +1,8 @@
+import time
+
+def timer(fn, *args, rep=1, **kwargs):
+    start = time.perf_counter()
+    for i in range(rep):
+        fn(*args, **kwargs)
+    end = time.perf_counter()
+    return end - start
